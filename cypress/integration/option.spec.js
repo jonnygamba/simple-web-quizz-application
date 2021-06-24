c5e75334702e40bd6ce2de6f489c721a7e5b495d
Jonny Gamba <jonnygalilei@gmail.com>
2021-06-24 13:13:15 -0500

    show questions from endpoint

--- cypress/integration/option.spec.js
+++ cypress/integration/option.spec.js
@@ -1,14 +1,28 @@
-describe("Test option", () => {
+describe("Every question should be valid", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  it("should have 4 options", () => {
-    cy.get("input[type=radio]").should("have.length", 4);
+  it("tests that every question has 4 radios", () => {
+    cy.get("fieldset").each(($el, index) => {
+      cy.wrap($el).find("input[type=radio]").should("have.length", 4);
+    });
   });
-  it("should have a valid question", () => {
-    const question = cy.get("legend");
-    question.should("have.length", 1);
-
-    question.first().invoke("text").its("length").should("be.gt", 5);
+  it('tests that every question has the title "question + {n}"', () => {
+    cy.get("fieldset").each(($el, index) => {
+      cy.wrap($el)
+        .find("legend")
+        .should("have.length", 1)
+        .and("text", `question ${index + 1}`);
+    });
+  });
+  it("tests that every question has a valid question", () => {
+    cy.get("fieldset").each(($el) => {
+      cy.wrap($el)
+        .get("fieldset")
+        .first()
+        .invoke("text")
+        .its("length")
+        .should("be.gt", 5);
+    });
   });
 });
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,48 @@
+import { useEffect, useState } from "react";
 import Question from "../src/components/question";
 import Option from "../src/components/answer";
 
 export default function Home() {
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/questions");
+        const result = await response.json();
+        setResults(result);
+      } catch (err) {
+        console.error(err);
+        setError("An error occurred");
+      }
+    };
+
+    fetchResults();
+  }, []);
+
+  if (error) {
+    return <span>{error}</span>;
+  }
+
   return (
     <form action="">
-      <Question question="question 1">
-        <Option id="answer-1" group="question-1" question="question 1?" />
-        <Option id="answer-2" group="question-1" question="question 2?" />
-        <Option id="answer-3" group="question-1" question="question 3?" />
-        <Option id="answer-4" group="question-1" question="question 4?" />
-      </Question>
+      {results.map((question, i) => {
+        return (
+          <Question key={question.text} question={`question ${i + 1}`}>
+            {question.options.map((option, j) => {
+              return (
+                <Option
+                  key={option.text}
+                  id={`answer-${i}${option.option}`}
+                  group={`question-${j}`}
+                  question={option.text}
+                />
+              );
+            })}
+          </Question>
+        );
+      })}
     </form>
   );
 }
